29a0c9c8d93c9e7361d89bd207b6d88290535057
Mcbeany <mcbeany7@gmail.com>
2022-06-25 20:58:59 +0700

    Update the code & add version name

--- src/Mcbeany/libAsync/libAsync.php
+++ src/Mcbeany/libAsync/libAsync.php
@@ -12,53 +12,53 @@ use SOFe\AwaitGenerator\Await;
 use Throwable;
 
 final class libAsync {
-    /**
-     * Call a function asynchronously.
-     *
-     * @template T
-     * @param Closure(): T @executor
-     *
-     * @return Generator<mixed, mixed, mixed, T>
-     */
-    public static function doAsync(Closure $executor): Generator {
-        $closure = function (Closure $resolve, Closure $reject) use ($executor): void {
-            $task = new class($executor, $resolve, $reject) extends AsyncTask {
-                private bool $rejected = false;
-                public function __construct(
-                    private Closure $executor,
-                    Closure $resolve,
-                    Closure $reject
-                ) {
-                    $this->storeLocal('resolve', $resolve);
-                    $this->storeLocal('reject', $reject);
-                }
+	/**
+	 * Call a function asynchronously.
+	 *
+	 * @template T
+	 * @param Closure(): T $executor
+	 *
+	 * @return Generator<mixed, mixed, mixed, T>
+	 */
+	public static function doAsync(Closure $executor): Generator {
+		$resolve = yield Await::RESOLVE;
+		$reject = yield Await::REJECT;
+		$task = new class($executor, $resolve, $reject) extends AsyncTask {
+			private bool $rejected = false;
+			public function __construct(
+				private Closure $executor,
+				Closure $resolve,
+				Closure $reject
+			) {
+				$this->storeLocal('resolve', $resolve);
+				$this->storeLocal('reject', $reject);
+			}
 
-                public function onRun(): void {
-                    try {
-                        $result = ($this->executor)();
-                    } catch (Throwable $e) {
-                        $this->rejected = true;
-                        $result = $e;
-                    }
-                    $this->setResult($result);
-                }
+			public function onRun(): void {
+				try {
+					$result = ($this->executor)();
+				} catch (Throwable $e) {
+					$this->rejected = true;
+					$result = $e;
+				}
+				$this->setResult($result);
+			}
 
-                public function onCompletion(): void {
-                    /** @var T|Throwable $result */
-                    $result = $this->getResult();
-                    if ($this->rejected && $result instanceof Throwable) {
-                        /** @var Closure(Throwable): void */
-                        $reject = $this->fetchLocal('reject');
-                        $reject($result);
-                        return;
-                    }
-                    /** @var Closure(T): void $resolve */
-                    $resolve = $this->fetchLocal('resolve');
-                    $resolve($result);
-                }
-            };
-            Server::getInstance()->getAsyncPool()->submitTask($task);
-        };
-        return yield from Await::promise($closure);
-    }
+			public function onCompletion(): void {
+				/** @var T|Throwable $result */
+				$result = $this->getResult();
+				if ($this->rejected && $result instanceof Throwable) {
+					/** @var Closure(Throwable): void */
+					$reject = $this->fetchLocal('reject');
+					$reject($result);
+					return;
+				}
+				/** @var Closure(T): void $resolve */
+				$resolve = $this->fetchLocal('resolve');
+				$resolve($result);
+			}
+		};
+		Server::getInstance()->getAsyncPool()->submitTask($task);
+		return yield Await::ONCE;
+	}
 }
